bcf0389292b5928335030391965969899e8e55e1
deknaa <74857225+deknaa@users.noreply.github.com>
2021-09-24 11:40:01 +0800

    Add files via upload

---
+++ persegi.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+// Luas Persegi : L = S x S
+// Keliling Persegi : K = 4 x S 	
+	
+	int pilihan, sisi1, sisi2;
+	
+	cout << "--- Silahkan Pilih ---" << endl;
+	cout << "1. Hitung Luas Persegi\n";
+	cout << "2. Keliling Persegi\n";
+	cout << "----------------------" << endl;
+	
+	cout << "Masukan pilihan anda : " << endl;
+	cin >> pilihan;
+	
+	if(pilihan == 1) {
+		cout << "--- Menghitung Luas Persegi ---\n";
+		cout << "Masukan sisi pertama :\n";
+		cin >> sisi1;
+		cout << "Masukan sisi kedua :\n";
+		cin >> sisi2;
+		cout << "Luas Persegi adalah : " << sisi1*sisi2;
+	}else if(pilihan == 2){
+		cout << "--- Menghitung Keliling Persegi ---\n";
+		cout << "Masukan sisi : \n";
+		cin >> sisi1;
+		cout << "Keliling persegi adalah :" << 4*sisi1;
+	}else{
+		cout << "Input yang anda masukan salah!";
+	}	
+	
+}
